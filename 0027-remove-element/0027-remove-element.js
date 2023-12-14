8bd90986d0f972a34302b19dd40741d8e4dff0a3
joanaBrit <140211258+joanaBrit@users.noreply.github.com>
2023-12-14 18:40:34 +0000

    Time: 51 ms (62.25%), Space: 42.3 MB (20.43%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+// “in-place” means that you have to change the original array, not a copy of it.
+// create two pointers, one to iterate over the array and one to keep track of the position
+
+let removeElement = function(nums, val) {
+    // start with the second pointer to zero
+    let k = 0
+    for (let i = 0; i < nums.length; i++){
+        // if the current element is not equal to the value.
+        if (nums[i] !== val){
+        // swap it with the element at the second pointer
+        nums[k] = nums[i]
+        // increment the new pointer
+        k++ 
+        }
+    }
+    return k
+};
