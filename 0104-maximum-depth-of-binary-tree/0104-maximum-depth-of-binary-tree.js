93b2015f944a4300040ef587e0c6b2d22d67fce0
joanaBrit <140211258+joanaBrit@users.noreply.github.com>
2023-12-21 12:29:03 +0000

    Time: 54 ms (83.86%), Space: 45.6 MB (22.13%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -12,14 +12,14 @@
  */
 // A binary tree’s maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
 var maxDepth = function(root) {
-    // treeNode(root, root)
-    
-     // function treeNode(left, right)
+        // if not root or null return 0
         if(!root || null){
             return 0
     }
+         // get the max of depth of the left and right subtrees.
          let left = maxDepth(root.left)
          let right = maxDepth(root.right)
+         // calculate the maximun of both depths plus one for the current node.
          let result = Math.max(left, right) + 1
          return result
 };
