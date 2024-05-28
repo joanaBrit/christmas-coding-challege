f51e7dd0d38364df22ac112e24aa933666a5dbb5
joanaBrit <140211258+joanaBrit@users.noreply.github.com>
2024-05-28 16:48:53 +0100

    Time: 89 ms (46.38%), Space: 49.8 MB (62.19%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,16 +3,22 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    
-    const numMap = new Map()
-    
-    for (let i =0; i< nums.length; i++) {
-       const complement = target - nums[i] 
-       if (numMap.has(complement)) {
-           return [numMap.get(complement), i]
-       }
-        numMap.set(nums[i], i)
+var twoSum = function (nums, target) {
+    // nums + nums = target
+    // nums = []
+    result = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            // console.log(i, j);
+            if(nums[i] + nums[j] === target){
+                
+                result.push(i);
+                result.push(j);
+                // console.log("found")
+                return result;
+                
+            }
+        }
     }
-    return null
 };
